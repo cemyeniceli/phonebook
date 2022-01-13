2a6adfbb70a22cf89fc628ed9ac9a576d8da3d8a
cemyeniceli <cemyeniceli@gmail.com>
2022-01-13 15:06:37 +0100

    updates for heroku deploy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('dataSent', (req) => {
@@ -9,6 +10,7 @@ morgan.token('dataSent', (req) => {
     return null
 })
 
+app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :dataSent'))
 app.use(express.json())
 
@@ -94,7 +96,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
